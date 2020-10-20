1fd5e10375f309e1fb2c3d24e6df63da874bff49
conanskyforce <conanskyforce@163.com>
2020-10-20 09:29:02 +0800

    feat: bubble

--- algo-101.js
+++ algo-101.js
@@ -394,6 +394,19 @@ var binarySearch = (num, arr) => {
 var arr = [1,3,4,5,6,87,754]
 binarySearch(5,arr)
 
+var bubbleSort = (arr) => {
+	for(let i = 0; i < arr.length; i++){
+		for(let j = i ; j < arr.length; j ++) {
+			if(arr[j] < arr[i]) {
+				[arr[j],arr[i] ] = [arr[i],arr[j] ]
+			}
+		}
+	}
+	return arr
+}
+
+
+
 
 
 
