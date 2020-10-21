026e8c94aef40736fd6a6cb20134d3e0b3d76a20
conanskyforce <conanskyforce@163.com>
2020-10-21 10:50:06 +0800

    feat: select sort

--- algo-101.js
+++ algo-101.js
@@ -405,6 +405,31 @@ var bubbleSort = (arr) => {
 	return arr
 }
 
+var selectSort = (arr) => {
+	let min
+	for(let i = 0; i < arr.length; i++){
+		min = i
+		for(let j = i + 1 ; j < arr.length; j ++) {
+			if(arr[j] < arr[min]) {
+				min = j
+			}
+		}
+		if(i != min) {
+			[arr[i],arr[min]] = [arr[min],arr[i]]
+		}
+	}
+	return arr
+}
+
+
+
+
+
+
+
+
+
+
 
 
 
