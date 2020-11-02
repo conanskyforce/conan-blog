6019aba18bafdef9b8ae1159fa0529da80b1818b
conanskyforce <conanskyforce@163.com>
2020-11-02 13:17:11 +0800

    feat: 缓动algo

--- design-pattern.js
+++ design-pattern.js
@@ -156,3 +156,62 @@ const calculateBonus = (level, salary) => {
 calculateBonus('A', 52000)
 calculateBonus('B', 32000)
 calculateBonus('C', 12000)
+
+// 缓动算法
+var tween = {
+  linear: function(t,b,c,d){
+    return c * t / d + b
+  },
+  easeIn: function(t,b,c,d){
+    return c * (t /= d ) * t + b
+  },
+  strongEaseIn: function(t,b,c,d) {
+    return c * ( t /= d) * t * t * t * t + b
+  },
+  strongEaseOut: function(t,b,c,d) {
+    return c * ((t = t / d - 1) * t *t *t *t + 1) + b
+  },
+  sineaseIn: function(t,b,c,d) {
+    return c * ( t /= d) * t * t + b
+  },
+  sineaseOut: function(t,b,c,d) {
+    return c * ((t = t / d - 1) * t *t + 1) + b
+  }
+}
+const Animate = function(dom) {
+  this.dom = dom // 动画dom节点
+  this.startTime = 0 // 动画开始时间
+  this.startPos = 0 // 动画开始时dom位置
+  this.endPos = 0 // 动画结束时dom位置
+  this.propertyName = null // 需要被改变的css属性名
+  this.easing = null // 缓动算法
+  this.duration = null // 动画持续时间
+}
+Animate.prototype.start = function(propertyName, endPos, duration, easing){
+  this.startTime = +new Date()
+  this.startPos = this.dom.getBoundingClientRect()[propertyName] // dom节点初始位置
+  this.endPos = endPos
+  this.propertyName = propertyName
+  this.easing = tween[easing]
+  this.duration = duration
+  const timer = setInterval(()=>{
+    if(this.step() === false){
+      clearInterval(timer)
+    }
+  }, 19)
+}
+Animate.prototype.step = function(){
+  const t = +new Date()
+  if (t >= this.startTime + this.duration) {
+    this.update(this.endPos)
+    return false
+  }
+  const pos = this.easing(t - this.startTime,this.startPos,this.endPos - this.startPos, this.duration)
+  this.update(pos)
+}
+Animate.prototype.update = function(pos){
+  this.dom.style[this.propertyName] = pos + 'px'
+}
+var div = document.querySelector('.source-box'); 
+var animate = new Animate( div );
+animate.start( 'top', 500, 2000, 'strongEaseOut' );
