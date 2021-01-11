a10eef12728248fcd5fe7e767c378ffda824f422
conanskyforce <conanskyforce@163.com>
2021-01-11 17:27:48 +0800

    feat: js aop

--- design-pattern.js
+++ design-pattern.js
@@ -798,3 +798,21 @@ document.querySelectorAll = function(...args){
   console.log(arguments)
   return _querySelectorAll.apply(document, arguments)
 }
+
+// ### 用 AOP 装饰函数
+
+Function.prototype.before = function(beforeFn){
+  const _self = this
+  return function(){
+    beforeFn.apply(this, arguments)
+    return _self.apply(this,arguments)
+  }
+}
+Function.prototype.after = function(afterFn){
+  const _self = this
+  return function(){
+    let ret = _self.apply(this,arguments)
+    afterFn.apply(this, arguments)
+    return ret
+  }
+}
