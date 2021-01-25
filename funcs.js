814eece755efb1a550455bc456fd03e8f62c977d
conanskyforce <conanskyforce@163.com>
2021-01-25 14:53:45 +0800

    feat: concurrency mapLimit

--- funcs.js
+++ funcs.js
@@ -1,5 +1,26 @@
 // record some design pattern, fun functions...
 
+const mapLimit = (tasks, concurrency = 5, cb, showProcess) => {
+  let index = 0,
+    running = 0,
+    completed = 0;
+  function nextTick() {
+    showProcess && (console.log(`running :${running + 1}, process: ${index + 1}/${tasks.length}`))
+    while (index < tasks.length - 1 && running < concurrency) {
+      const task = tasks[index++]
+      running++
+      task(() => {
+        running--
+        completed++
+        if (completed >= tasks.length) {
+          return cb()
+        }
+        nextTick()
+      })
+    }
+  }
+  nextTick()
+}
 
 const btoa = (str) => Buffer.from(str, 'binary').toString('base64')
 const atob = (str) => Buffer.from(str, 'base64').toString('utf-8')
